1c1e11d2b2c5943172a7e05131ed13951488d3bb
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2019-10-28 04:43:28 +0600

    added HW 5

---
+++ M4. Framework Fundamentals/Framework Fundamentals Problems/Problem5/Problem5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace Problem5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "The greatest victory is that which requires no battle";
+            string res = ReverseWords(s);
+            Console.WriteLine(res);
+        }
+
+        public static string ReverseWords(string s) 
+        {
+            if (string.IsNullOrEmpty(s))
+                throw new ArgumentException("string cannot be null or empty");
+
+            string[] words = s.Split(' ');
+            var sb = new StringBuilder();
+            for (int i = words.Length - 1; i >= 0; i--) 
+            {
+                sb.Append(words[i]);
+                if (i != 0)
+                    sb.Append(" ");
+            }
+
+            return sb.ToString();
+                
+        }
+    }
+}
