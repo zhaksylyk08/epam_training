d55353116da705aba28753b7333a3f4bf2e9abf5
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2019-10-27 23:06:43 +0600

    added new HW solution

---
+++ M4. Framework Fundamentals/Framework Fundamentals Problems/Problem1/Problem1/Customer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Problem1
+{
+    public class Customer
+    {
+        public string Name { get; private set; }
+        public decimal Revenue { get; private set; }
+        public string ContactPhone { get; private set; }
+
+        public Customer(string name, decimal revenue, string contactPhone) 
+        {
+            Name = name;
+            Revenue = revenue;
+            ContactPhone = contactPhone;
+        }
+
+        public override string ToString()
+        {
+            return ToString("A");
+        }
+
+        public string ToString(string format) 
+        {
+            if (string.IsNullOrEmpty(format))
+                format = "A";
+            string revenueToFormatString = string.Format(CultureInfo.InvariantCulture, "{0:#,#0.00}", Revenue);
+            switch (format.ToUpperInvariant()) 
+            {
+                case "A":
+                    return string.Format("Customer record: {0}, {1}, {2}", Name, revenueToFormatString,
+                                          ContactPhone);
+                case "B":
+                    return string.Format("Customer record: {0}", ContactPhone);
+                case "C":
+                    return string.Format("Customer record: {0}, {1}", Name, revenueToFormatString);
+                case "D":
+                    return string.Format("Customer record: {0}", Name);
+                case "E":
+                    return string.Format("Customer record: {0}", revenueToFormatString);
+                default:
+                    string message = string.Format("{0} is an invalid format string", format);
+                    throw new ArgumentException(message);
+            }
+        }
+    }
+}
---
+++ M4. Framework Fundamentals/Framework Fundamentals Problems/Problem1/Problem1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Customer("Jeffrey Richter", (decimal)1000000.95, "+1(425)555-0100");
+            var test2 = new Customer("Jackie Chan", 123000055, "+1(000)456-112");
+            Console.WriteLine(test);
+            Console.WriteLine(test.ToString("C"));
+            Console.WriteLine(test2.ToString("c"));
+        }
+    }
+}
