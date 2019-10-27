f79e9b802ce355afbc7f128d4cee4a50a6ef7fb1
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2019-10-28 03:54:39 +0600

    added unit test

--- M4. Framework Fundamentals/Framework Fundamentals Problems/Problem3/Problem3TestProject/UnitTest1.cs
+++ M4. Framework Fundamentals/Framework Fundamentals Problems/Problem3/Problem3TestProject/UnitTest1.cs
@@ -1,13 +1,22 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem3;
 
 namespace Problem3TestProject
 {
     [TestClass]
-    public class UnitTest1
+    public class ProgramTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddOrChangeUrlParameterTest()
         {
+            //Arrange
+            string url = "www.example.com?key=value";
+            string keyValueParameters = "key2=value2";
+            string expected = "www.example.com?key=value&key2=value2";
+            //Act
+            string actual = Program.AddOrChangeUrlParameter(url, keyValueParameters);
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
