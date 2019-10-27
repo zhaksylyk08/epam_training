68ac1649ea7ef85d96b76867905aa0d2f94b3ba4
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2019-10-28 05:57:43 +0600

    added new HW

---
+++ M4. Framework Fundamentals/Delegates. Lambdas and Events Problems/FFProblem1/FFProblem1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FFProblem1
+{
+    class Program
+    {
+        delegate int Operation(int x, int y);
+        static void Main(string[] args)
+        {
+            Operation del = FindGCDForTwoNums;
+            int result = del.Invoke(10, 20);
+            Console.WriteLine(result);
+        }
+
+        public static int FindGCDForTwoNums(int a, int b)
+        {
+            if (a == 0)
+                return b;
+            return FindGCDForTwoNums(b % a, a);
+        }
+        public static int FindGCD(params int[] nums)
+        {
+            int result = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                result = FindGCDForTwoNums(nums[i], result);
+            }
+
+            return result;
+        }
+    }
+}
