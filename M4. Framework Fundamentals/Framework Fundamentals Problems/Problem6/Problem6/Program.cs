5ebb811a8dd9f64ad60399522fba4f409176e892
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2019-10-28 05:06:28 +0600

    added new HW 6

---
+++ M4. Framework Fundamentals/Framework Fundamentals Problems/Problem6/Problem6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+namespace Problem6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+        }
+
+        public static string AddTwoBigNumbers(string bigNumber1, string bigNumber2) 
+        {
+            BigInteger num1 = 0, num2 = 0, res = 0;
+            BigInteger.TryParse(bigNumber1, out num1);
+            BigInteger.TryParse(bigNumber2, out num2);
+            res = num1 + num2;
+
+            return res.ToString();
+        }
+    }
+}
