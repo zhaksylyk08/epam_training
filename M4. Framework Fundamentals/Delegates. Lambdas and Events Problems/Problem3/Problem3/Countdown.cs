5f36580203d048cb3fd8027104de72d4e596aaa9
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2019-10-28 06:20:58 +0600

    another one

---
+++ M4. Framework Fundamentals/Delegates. Lambdas and Events Problems/Problem3/Problem3/Countdown.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem3
+{
+    public class Countdown
+    {
+        delegate void AccountHandler(string message);
+        event AccountHandler Notify;
+
+        
+    }
+}
