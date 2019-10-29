faf08aa32d8d297ba62a119922a35d06aa3a2ab9
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2019-10-29 20:26:40 +0600

    added generic stack

---
+++ M4. Framework Fundamentals/Generics and Collections Problems/GenericStackImplementation/MyGenericStack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericStackImplementation
+{
+    class MyGenericStack<T>: IEnumerable<T>
+    {
+        private T[] stack;
+        private int size,
+                    capacity = 10,
+                    current;
+
+        public int Count { get { return size; } }
+
+        public MyGenericStack() 
+        {
+            stack = new T[capacity];
+            size = 0;
+            current = -1;
+        }
+        public void Push(T data) 
+        {
+            if (size == capacity) 
+            {
+                var newStack = new T[capacity * 2];
+                for (int i = 0; i < size; i++)
+                    newStack[i] = stack[i];
+                stack = newStack;
+
+                capacity *= 2;
+            }
+
+            current++;
+            stack[current] = data;
+            
+            size++;
+        }
+
+        public T Pop() 
+        {
+            T el = stack[current];
+            current--;
+            size--;
+
+            return el;
+        }
+
+        public T Peek() => stack[current];
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = size - 1; i >= 0; i--)
+                yield return stack[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ M4. Framework Fundamentals/Generics and Collections Problems/GenericStackImplementation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericStackImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new MyGenericStack<int>();
+            test.Push(1);
+            test.Push(2);
+            test.Push(3);
+            //test.Pop();
+            foreach (int i in test)
+                Console.Write("{0} ", i);
+        }
+    }
+}
