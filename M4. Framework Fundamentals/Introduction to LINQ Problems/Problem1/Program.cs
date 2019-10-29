6dbd37f91266611ae9e6f6474af2a7b9221e627c
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2019-10-30 02:33:06 +0600

    added linq solution

---
+++ M4. Framework Fundamentals/Introduction to LINQ Problems/Problem1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for simplifications I used dictionary 
+            var testResults = new Dictionary<string, double>();
+            testResults.Add("Nanny", 23);
+            testResults.Add("Becky", 36);
+            testResults.Add("Ashley", 45);
+
+            var selectedStudents = from i in testResults
+                                   where i.Value > 30
+                                   select i;
+
+            foreach (var i in selectedStudents)
+                Console.WriteLine("Student: {0}, test score = {1}", i.Key, i.Value);
+        }
+    }
+}
