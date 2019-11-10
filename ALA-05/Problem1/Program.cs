acbd005b9f7f9117e175d5808b22d854676f3114
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2019-11-11 03:51:23 +0600

    added some changes

---
+++ ALA-05/Problem1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationTokenSource cts2 = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+            CancellationToken token2 = cts2.Token;
+
+            Console.WriteLine("Please, enter integer n:");
+            int n = Int32.Parse(Console.ReadLine());
+            GetSumOfRangeAsync(n, token);
+
+            int n2 = Int32.Parse(Console.ReadLine());
+            cts.Cancel();
+            GetSumOfRangeAsync(n2, token2);
+            //Console.ReadKey();
+        }
+        static void GetSumOfRange(int n, CancellationToken token) 
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Operation is cancelled");
+                    return;
+                }
+                sum += i;
+                Console.WriteLine("sum from 0 to {0} = {1}", i, sum);
+                Thread.Sleep(3000);
+            }
+            Console.WriteLine("sum = {0}", sum);
+        }
+        static async void GetSumOfRangeAsync(int n, CancellationToken token)
+        {
+            if (token.IsCancellationRequested)
+                return;
+            await Task.Run(() => GetSumOfRange(n, token));
+        }
+    }
+}
