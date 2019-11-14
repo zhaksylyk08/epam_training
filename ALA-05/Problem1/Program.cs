01b75f771fb0cdea076fdd1099278a99a690ff70
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2019-11-14 23:58:32 +0600

    added some after feedback

--- ALA-05/Problem1/Program.cs
+++ ALA-05/Problem1/Program.cs
@@ -11,41 +11,32 @@ namespace Problem1
     {
         static void Main(string[] args)
         {
-            CancellationTokenSource cts = new CancellationTokenSource();
-            CancellationTokenSource cts2 = new CancellationTokenSource();
-            CancellationToken token = cts.Token;
-            CancellationToken token2 = cts2.Token;
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
 
-            Console.WriteLine("Please, enter integer n:");
-            int n = Int32.Parse(Console.ReadLine());
-            GetSumOfRangeAsync(n, token);
+            var cts2 = new CancellationTokenSource();
+            var token2 = cts2.Token;
 
-            int n2 = Int32.Parse(Console.ReadLine());
-            cts.Cancel();
-            GetSumOfRangeAsync(n2, token2);
-            //Console.ReadKey();
-        }
-        static void GetSumOfRange(int n, CancellationToken token) 
-        {
-            int sum = 0;
-            for (int i = 1; i <= n; i++)
+            int n, n2;
+
+            Console.WriteLine("Enter integer number:");
+      
+            if (Int32.TryParse(Console.ReadLine(), out n) && n > 0)
             {
-                if (token.IsCancellationRequested)
-                {
-                    Console.WriteLine("Operation is cancelled");
-                    return;
-                }
-                sum += i;
-                Console.WriteLine("sum from 0 to {0} = {1}", i, sum);
-                Thread.Sleep(3000);
+                var s = new Sum();
+                s.GetSumOfRangeAsync(n, token);
             }
-            Console.WriteLine("sum = {0}", sum);
+
+            if (Int32.TryParse(Console.ReadLine(), out n2) && n2 > 0)
+            {
+                cts.Cancel();
+                var s2 = new Sum();
+                s2.GetSumOfRangeAsync(n2, token2);
+            }
+
+            Console.ReadKey();
         }
-        static async void GetSumOfRangeAsync(int n, CancellationToken token)
-        {
-            if (token.IsCancellationRequested)
-                return;
-            await Task.Run(() => GetSumOfRange(n, token));
-        }
+
+
     }
 }
---
+++ ALA-05/Problem1/Sum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    public class Sum
+    {
+        private int GetSumOfRange(int n, CancellationToken token)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("The operation is cancelled");
+                    return -1;
+                }
+                sum += i;
+                Thread.Sleep(1000);
+            }
+
+            return sum;
+        }
+
+        public async void GetSumOfRangeAsync(int n, CancellationToken token)
+        {
+            if (token.IsCancellationRequested) 
+            {
+                Console.WriteLine("The operation is cancelled");
+                return;
+            }
+            int x = await Task.Run(() => GetSumOfRange(n, token));
+            //Thread.Sleep(5000);
+            if(x > -1)
+                Console.WriteLine(x);
+        }
+    }
+}
