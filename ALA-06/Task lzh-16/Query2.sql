f3f33630620d810984714ba8d60aa8addebcc2cd
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2019-11-22 01:04:25 +0600

    changed again

--- ALA-06/Task lzh-16/Query2.sql
+++ ALA-06/Task lzh-16/Query2.sql
@@ -44,13 +44,11 @@ SELECT DISTINCT OrderID
 FROM [Order Details] WHERE Quantity BETWEEN 3 AND 10;
 
 --3.2 
---Запрос: стоимость запроса (по отношению к пакету): 50%
 SELECT CustomerID, Country
 FROM Customers WHERE SUBSTRING(Country, 1, 1) BETWEEN 'B' AND 'G'
 ORDER BY Country;
 
 --3.3
---Запрос: стоимость запроса(по отношению к пакету): 50%
 SELECT CustomerID, Country
 FROM Customers WHERE SUBSTRING(Country, 1, 1) >= 'B' AND SUBSTRING(Country, 1, 1) <= 'G'
 ORDER BY Country;
@@ -63,37 +61,37 @@ FROM Products WHERE ProductName LIKE '%cho_olade%';
 
 --5
 
---5.1 Найти общую сумму всех заказов из таблицы Order Details с учетом количества закупленных товаров и скидок по ним. 
+--5.1  
 SELECT ROUND(SUM((UnitPrice - Discount*UnitPrice)*Quantity), 2) AS 'Totals'
 FROM [Order Details];
 
---5.2 По таблице Orders найти количество заказов, которые еще не были доставлены (т.е. в колонке ShippedDate нет значения даты доставки). 
+--5.2 
 SELECT COUNT(CASE WHEN ShippedDate IS NULL THEN 1 END)
 FROM Orders;
 
---5.3 По таблице Orders найти количество различных покупателей (CustomerID), сделавших заказы. 
+--5.3
 SELECT COUNT(DISTINCT CustomerID)
 FROM Orders;
 
 --6
 
---6.1 По таблице Orders найти количество заказов с группировкой по годам. В результатах запроса надо высвечивать две колонки c названиями Year и Total. 
+--6.1 
 SELECT YEAR(ShippedDate) AS Year, COUNT(OrderID) AS Total
 FROM Orders GROUP BY YEAR(ShippedDate);
 
---6.2 По таблице Orders найти количество заказов, cделанных каждым продавцом. 
+--6.2 П
 SELECT CONCAT(LastName, ' ', FirstName) AS 'Seller', Amount
 FROM Employees emp
 INNER JOIN(SELECT EmployeeID, COUNT(OrderID) AS Amount FROM Orders GROUP BY(EmployeeID)) ord
 ON ord.EmployeeID = emp.EmployeeID
 ORDER BY Amount DESC; 
 
---6.3 По таблице Orders найти количество заказов, cделанных каждым продавцом и для каждого покупателя
+--6.3 
 SELECT EmployeeID, CustomerID, COUNT(OrderID) AS 'Amount'
 FROM Orders 
 GROUP BY CUBE(EmployeeID, CustomerID);
 
---6.4 Найти покупателей и продавцов, которые живут в одном городе. 
+--6.4  
 SELECT CONCAT(LastName, ' ', FirstName) AS Person, 'Seller' AS Type, City
 FROM Employees AS emp
 WHERE EXISTS(SELECT * FROM Customers AS c
@@ -105,13 +103,13 @@ WHERE EXISTS(SELECT * FROM Employees AS emp
 				WHERE emp.City = c.City)
 ORDER BY City, Person;
 
---6.5 Найти всех покупателей, которые живут в одном городе. В запросе использовать соединение таблицы Customers c собой - самосоединение. 
+--6.5 
 SELECT CustomerID, City
 FROM Customers AS c
 WHERE EXISTS(SELECT * FROM Customers AS c2
 				WHERE c.City = c2.City AND NOT(c.CustomerID = c2.CustomerID));
 
---6.6 По таблице Employees найти для каждого продавца его руководителя, т.е. кому он делает репорты. Высветить 
+--6.6  
 SELECT LastName AS 'User Name', Boss
 FROM Employees AS emp
 INNER JOIN (SELECT  EmployeeID, LastName AS Boss FROM Employees) AS emp2
@@ -119,7 +117,7 @@ ON emp2.EmployeeID = emp.ReportsTo;
 
 --7
 
---7.1 Определить продавцов, которые обслуживают регион 'Western' (таблица Region). 
+--7.1  
 SELECT LastName, TerritoryDescription AS Territory
 FROM Employees as emp
 INNER JOIN EmployeeTerritories AS empter ON emp.EmployeeID = empter.EmployeeID
@@ -129,7 +127,7 @@ WHERE reg.RegionDescription = 'Western';
 
 --8
 
---8.1 Высветить в результатах запроса имена всех заказчиков из таблицы Customers и суммарное количество их заказов из таблицы Orders. 
+--8.1 
 SELECT c.CustomerID, COUNT(OrderID) 
 FROM Customers AS c
 LEFT OUTER JOIN Orders AS ord
@@ -139,7 +137,7 @@ ORDER BY COUNT(OrderID);
 
 --9
 
---9.1 Высветить всех поставщиков колонка CompanyName в таблице Suppliers, у которых нет хотя бы одного продукта на складе. 
+--9.1 
 SELECT CompanyName
 FROM Suppliers AS s
 JOIN Products AS p ON s.SupplierID = p.SupplierID
@@ -147,7 +145,7 @@ WHERE UnitsInStock IN(SELECT UnitsInStock = 0 FROM Products);
 
 --10
 
---10.1 Высветить всех продавцов, которые имеют более 150 заказов. Использовать вложенный коррелированный SELECT
+--10.1 
 SELECT CONCAT(LastName, ' ', FirstName)
 FROM Employees
 WHERE EmployeeID IN
@@ -157,7 +155,7 @@ WHERE EmployeeID IN
 
 --11
 
---11.1 Высветить всех заказчиков (таблица Customers), которые не имеют ни одного заказа (подзапрос по таблице Orders).
+--11.1 
 SELECT ContactName
 FROM Customers AS c
 WHERE NOT EXISTS(SELECT CustomerID FROM Orders AS ord WHERE c.CustomerID = ord.CustomerID  
@@ -166,7 +164,7 @@ WHERE NOT EXISTS(SELECT CustomerID FROM Orders AS ord WHERE c.CustomerID = ord.C
 
 --12
 
---12.1 Для формирования алфавитного указателя Employees высветить из таблицы Employees список только тех букв алфавита, с которых начинаются фамилии Employees (колонка LastName ) из этой таблицы. 
+--12.1 
 SELECT SUBSTRING(LastName, 1, 1) AS 'First Letter'
 FROM Employees
 ORDER BY [First Letter];
