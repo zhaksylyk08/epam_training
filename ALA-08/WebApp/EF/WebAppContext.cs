36e89332ec5000360de9dcdaeaa9bd0bef22ebf5
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2019-12-02 07:20:20 +0600

    added some changes

---
+++ ALA-08/WebApp/EF/WebAppContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.EF
+{
+    public class WebAppContext: DbContext
+    {
+        public WebAppContext(DbContextOptions<WebAppContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Award> Awards { get; set; }
+    }
+}
