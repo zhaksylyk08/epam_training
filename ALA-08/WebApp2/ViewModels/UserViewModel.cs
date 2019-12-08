5fc708d47de296979ad5828c877b2d9a88ba5554
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2019-12-09 01:12:45 +0600

    added data annotations to VM

---
+++ ALA-08/WebApp2/ViewModels/UserViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp2.ViewModels
+{
+    public class UserViewModel
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }    // User
+
+        [DataType(DataType.Date)]
+
+        [Required]
+        public DateTime Birthdate { get; set; }
+
+        public int Age { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int AwardId { get; set; }     // Award
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        public string AwardImageUrl{get; set;}
+    }
+}
